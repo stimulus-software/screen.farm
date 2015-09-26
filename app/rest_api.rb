3ff04fad2764afae47a10aa339cac964ea171862
Levente Bagi <bagilevi@gmail.com>
2015-09-26 15:24:34 +0100

    remove 'all' option

--- app/rest_api.rb
+++ app/rest_api.rb
@@ -2,23 +2,16 @@ require 'sinatra/base'
 
 class RestApi < Sinatra::Base
   post '/c/:channel' do
-    if params[:channel] == 'all'
-      $channel_registry.all_active.each do |channel|
-        channel.show(url: params[:url])
-      end
-      "OK\n"
+    channel = $channel_registry[params[:channel].to_s]
+    if ! channel
+      status 404
+      "channel_not_found #{params[:channel].inspect}\n"
+    elsif ! channel.active?
+      status 403
+      "channel_not_active #{params[:channel].inspect}\n"
     else
-      channel = $channel_registry[params[:channel].to_s]
-      if ! channel
-        status 404
-        "channel_not_found #{params[:channel].inspect}\n"
-      elsif ! channel.active?
-        status 403
-        "channel_not_active #{params[:channel].inspect}\n"
-      else
-        channel.show(url: params[:url])
-        "OK\n"
-      end
+      channel.show(url: params[:url])
+      "OK\n"
     end
   end
 
